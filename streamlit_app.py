d9778cb30c138f0b32fceb74b43d67f436e6273a
wijenpasar <wijenpasar@gmail.com>
2026-05-22 10:33:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 hendrokembung")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+left, middle, right = st.columns(3)
+if left.button("Plain button", width="stretch"):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", width="stretch"):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", width="stretch"):
+    right.markdown("You clicked the Material button.")
